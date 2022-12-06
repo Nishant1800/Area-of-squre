493e3d13d7fbacfc5edb495ae7182ded4339b163
Nishant Ranjan <110740001+Nishant1800@users.noreply.github.com>
2022-12-07 00:30:33 +0530

    Add files via upload

---
+++ Hello2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// area of squre
+int main()
+{
+    float side;
+    print("enter side");
+    scanf("%d", &side);
+
+    printf("area is : %f", side * side);
+    return 0;
+
+}
